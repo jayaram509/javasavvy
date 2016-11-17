dc1d3fe0165108a524e2c19b780415a7cb212b4c
jayaram <jayaram.pokuri@igate.com>
2016-11-17 15:28:43 +0530

    latest changes

--- liferay-jersy-portlet/docroot/WEB-INF/src/org/javasavvy/rest/controller/ProductController.java
+++ liferay-jersy-portlet/docroot/WEB-INF/src/org/javasavvy/rest/controller/ProductController.java
@@ -1,10 +1,15 @@
 package org.javasavvy.rest.controller;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.javasavvy.rest.modal.ProductModal;
+import org.javasavvy.rest.modal.StatusModal;
 
 @Path(value="/product")
 public class ProductController {
@@ -12,6 +17,7 @@ public class ProductController {
 	
 	@GET
 	@Path("/getProductDetails")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getProductDetails(){
 		
 		ProductModal pr = new ProductModal();
@@ -20,5 +26,16 @@ public class ProductController {
 		pr.setProductId(1L);
 		System.out.println("ehits ");
 		return Response.status(200).entity(pr).build();
+	}
+	
+	
+	@POST
+	@Path("/addProduct")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addProduct(ProductModal pr){
+		StatusModal st = new StatusModal();
+		st.setMessage("Product Created Successfully");
+		st.setStatus(200);
+		return Response.status(200).entity(st).build();
 	}
 }
